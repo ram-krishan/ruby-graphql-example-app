110f391d4aa306509097a959a6c8bbf185ec0273
Ramkrishan patidar <ramkrishan.rails@gmail.com>
2018-04-26 18:37:12 +0530

    mutation: createUser, updateUser and deleteUser

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,7 +5,45 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :testField, types.String do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
-      "Hello World!"
+      "mutation Hello World!"
     }
+  end
+
+  #delete user.
+  field :deleteUser, Types::UserType do
+    argument :id, types.Int
+    description "User delete"
+    resolve ->(obj, args, ctx) { User.find(args.id).destroy }
+  end
+
+  #delete user.
+  field :createUser, Types::UserType do
+    argument :first_name, !types.String
+    argument :last_name, !types.String
+    argument :email, !types.String
+    argument :mobile_number, types.String
+    argument :password, !types.String
+
+    description "Create user"
+    resolve ->(obj, args, ctx)  do
+      User.create!(args.to_h)
+    end
+  end
+
+    #delete user.
+  field :updateUser, Types::UserType do
+    argument :id, !types.Int
+    argument :first_name, types.String
+    argument :last_name, types.String
+    argument :email, types.String
+    argument :mobile_number, types.String
+    argument :password, types.String
+
+    description "Update user"
+    resolve ->(obj, args, ctx)  do
+      user = User.find(args.id)
+      user.update(args.to_h)
+      user
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,5 +1,6 @@
 Types::UserType = GraphQL::ObjectType.define do
   name "User"
+  field :id, types.Int
   field :first_name, types.String
   field :last_name, types.String
   field :email, types.String
