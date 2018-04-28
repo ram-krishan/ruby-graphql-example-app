fa035ffb96361d4138b3ffa6d14802a22b003e16
Ramkrishan patidar <ramkrishan.rails@gmail.com>
2018-04-28 13:19:06 +0530

    context: current user in graphql and remove unwanted code

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,11 +4,4 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :deleteUser, Mutations::DeleteUser.field
   field :updateUser, Mutations::UpdateUser.field
   field :createUser, Mutations::CreateUser.field
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "mutation Hello World!"
-    }
-  end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,18 +3,10 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
-
   #listing all the users.
   field :users, types[Types::UserType] do
     argument :page, types.Int, default_value: 1
-    argument :per_page, types.Int, default_value: 1
+    argument :per_page, types.Int, default_value: 20
 
     description "User List"
     resolve ->(obj, args, ctx) do
@@ -27,5 +19,11 @@ Types::QueryType = GraphQL::ObjectType.define do
     argument :id, types.Int
     description "User detail"
     resolve ->(obj, args, ctx) { User.find(args.id) }
+  end
+
+  #user detail
+  field :userProfile, Types::UserType do
+    description "User detail"
+    resolve ->(obj, args, ctx) { ctx[:current_user] }
   end
 end
